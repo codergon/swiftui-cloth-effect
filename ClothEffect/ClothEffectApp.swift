ba163d74f7b07e6337e43d953dac58202f050deb
codergon <atakerewilliams@gmail.com>
2025-01-30 22:27:04 +0100

    Initial Commit

---
+++ ClothEffect/ClothEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothEffectApp.swift
+//  ClothEffect
+//
+//  Created by Kester Atakere on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClothEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
